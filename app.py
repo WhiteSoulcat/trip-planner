6df74b19cf9a83212542a1f9dc4388939bb54745
WhiteSoulcat <87729189+WhiteSoulcat@users.noreply.github.com>
2026-02-13 15:59:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -807,5 +807,6 @@ def plan():
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
